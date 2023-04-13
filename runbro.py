65f98ff4b924d9158dc2cd576730ba61af04d226
Akash Raina <kevinraina279@gmail.com>
2023-04-13 12:24:13 +0530

    Added new Characters in-game

--- runbro.py
+++ runbro.py
@@ -9,6 +9,17 @@ pygame.display.set_caption("RunBro")
 ground_surface = pygame.image.load("Graphics/ground.png").convert()
 sky_surface = pygame.image.load("Graphics/Sky.png").convert()
 
+# Player_surface
+player_surface = pygame.image.load("Graphics/Player/player_stand.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (100, 300))
+
+# Snail_surface 
+snail_surface = pygame.image.load("Graphics/Snail/snail1.png").convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+
+# Clock
+clock = pygame.time.Clock()
+
 while(True):
 
     for event in pygame.event.get():
@@ -19,4 +30,12 @@ while(True):
     # Background_surface
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
+
+    # Player_surface
+    screen.blit(player_surface, player_rect)
+
+    # Snail_surface
+    screen.blit(snail_surface,snail_rect)
+
     pygame.display.update()
+    clock.tick(60)
